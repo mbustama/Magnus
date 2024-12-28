445a754ef729c493282741f568326c845054ad10
mbustama <mbustamante@gmail.com>
2024-12-28 22:44:28 +0100

    Edited the Simpson's integration routine for speed-up, and removed if-else evaluations of of integration_method at each order of the Magnus expansion

--- magnus/magnus.py
+++ magnus/magnus.py
@@ -63,20 +63,29 @@ def compute_magnus_terms(A: np.ndarray, t0: float, t1: float, n_tpts: Optional[i
     def commutator(X: np.ndarray, Y: np.ndarray) -> np.ndarray:
         return X @ Y - Y @ X
 
-    def integral_cumulative_simpson(times: np.ndarray, matrices: np.ndarray) -> np.ndarray:
-        # We need to write our custom routine to compute cumulative matrix integrals because the
-        # scipy routine `integrate.cumulative_simpson` does not handle complex numbers; it casts
-        # them into real numbers.  This is not a problem of the `integrate.cumulative_trapezoid`, 
-        # which we do use.
-        result = []
-        for i in range(len(times)):
-            if (i == 0): # Integral from t0 to t0
-                result.append(zero_matrix)
-            elif (i == 1): # Integral from t0 to t1
-                result.append(0.5*(times[1]-times[0])*sum(matrices[:2]))
-            else:
-                result.append(sp.integrate.simpson(matrices[0:i+1], x=times[0:i+1], axis=0))
-        return np.array(result)
+    # def integral_cumulative_simpson(times: np.ndarray, matrices: np.ndarray) -> np.ndarray:
+    #     # We need to write our custom routine to compute cumulative matrix integrals because the
+    #     # scipy routine `integrate.cumulative_simpson` does not handle complex numbers; it casts
+    #     # them into real numbers.  This is not a problem of the `integrate.cumulative_trapezoid`, 
+    #     # which we do use.
+    #     result = []
+    #     for i in range(len(times)):
+    #         if (i == 0): # Integral from t0 to t0
+    #             result.append(zero_matrix)
+    #         elif (i == 1): # Integral from t0 to t1
+    #             result.append(0.5*(times[1]-times[0])*sum(matrices[:2]))
+    #         else:
+    #             result.append(sp.integrate.simpson(matrices[0:i+1], x=times[0:i+1], axis=0))
+    #     return np.array(result)
+
+    def integral_cumulative_simpson(matrices: np.ndarray, x: np.ndarray, **kwargs) -> np.ndarray:
+        return np.array([sp.integrate.simpson(matrices[:i + 1], x=x[:i + 1], axis=0) \
+            for i in range(len(times))])
+
+    if integration_method == 'trapezoid':
+        integral_cumulative = sp.integrate.cumulative_trapezoid 
+    elif integration_method == 'simpson':
+        integral_cumulative = integral_cumulative_simpson
 
     magnus_terms = []
 
@@ -84,21 +93,23 @@ def compute_magnus_terms(A: np.ndarray, t0: float, t1: float, n_tpts: Optional[i
     At = np.array([A(t) for t in times])
 
     # Precompute the Omega_1(t) terms, integrating from t0 to t = t0, ..., t1
-    if (integration_method == 'trapezoid'):
-        o1t = sp.integrate.cumulative_trapezoid(At, x=times, axis=0, initial=0)
-    elif (integration_method == 'simpson'):
-        # o1t = sp.integrate.cumulative_simpson(At, x=times, axis=0, initial=0)
-        o1t = integral_cumulative_simpson(times, At)
+    o1t = integral_cumulative(At, x=times, axis=0, initial=0)
+    # if (integration_method == 'trapezoid'):
+    #     o1t = sp.integrate.cumulative_trapezoid(At, x=times, axis=0, initial=0)
+    # elif (integration_method == 'simpson'):
+    #     # o1t = sp.integrate.cumulative_simpson(At, x=times, axis=0, initial=0)
+    #     o1t = integral_cumulative_simpson(At, times)
     magnus_terms.append(o1t[-1]) # Integral from t0 to t1
 
     # Precompute the Omega_2(t) terms, integrating from t0 to t = t0, ..., t1
     if order >= 2:
         o2t_integrand = -0.5 * np.stack([commutator(o1t[i], At[i]) for i in range(n_tpts)], axis=0)
-        if (integration_method == 'trapezoid'):
-            o2t = sp.integrate.cumulative_trapezoid(o2t_integrand, x=times, axis=0, initial=0)
-        elif (integration_method == 'simpson'):
-            # o2t = sp.integrate.cumulative_simpson(o2t_integrand, x=times, axis=0, initial=0)
-            o2t = integral_cumulative_simpson(times, o2t_integrand)
+        o2t = integral_cumulative(o2t_integrand, x=times, axis=0, initial=0)
+        # if (integration_method == 'trapezoid'):
+        #     o2t = sp.integrate.cumulative_trapezoid(o2t_integrand, x=times, axis=0, initial=0)
+        # elif (integration_method == 'simpson'):
+        #     # o2t = sp.integrate.cumulative_simpson(o2t_integrand, x=times, axis=0, initial=0)
+        #     o2t = integral_cumulative_simpson(o2t_integrand, times)
         magnus_terms.append(o2t[-1])
 
     # Precompute the Omega_3(t) terms, integrating from t0 to t = t0, ..., t1
@@ -108,31 +119,14 @@ def compute_magnus_terms(A: np.ndarray, t0: float, t1: float, n_tpts: Optional[i
             [commutator(o1t[i], commutator(o1t[i], At[i])) for i in range(n_tpts)], axis=0
         )
         o3t_integrand = t1 + t2
-        if (integration_method == 'trapezoid'):
-            o3t = sp.integrate.cumulative_trapezoid(o3t_integrand, x=times, axis=0, initial=0)
-        elif (integration_method == 'simpson'):
-            # o3t = sp.integrate.cumulative_simpson(o3t_integrand, x=times, axis=0, initial=0)
-            o3t = integral_cumulative_simpson(times, o3t_integrand)
+        o3t = integral_cumulative(o3t_integrand, x=times, axis=0, initial=0)
+        # if (integration_method == 'trapezoid'):
+        #     o3t = sp.integrate.cumulative_trapezoid(o3t_integrand, x=times, axis=0, initial=0)
+        # elif (integration_method == 'simpson'):
+        #     # o3t = sp.integrate.cumulative_simpson(o3t_integrand, x=times, axis=0, initial=0)
+        #     o3t = integral_cumulative_simpson(o3t_integrand, times)
         # The term in the Magnus expansion is the last one, which goes from t0 to t1
         magnus_terms.append(o3t[-1])
-        # ------------------------------------------------------------------------------------------
-        # Deprecated (slower):
-        # t1 = -0.5 * np.array([commutator(o2t[i], At[i]) for i in range(n_tpts)])
-        # t2 = (1.0/12.0) * np.array([commutator(o1t[i], commutator(o1t[i], At[i])) for i in range(n_tpts)])
-        # o3t_integrand = t1+t2
-        # o3t = [matrix_integral(times[0:i+1], o3t_integrand[0:i+1]) for i in range(n_tpts)]
-        # magnus_terms.append(o3t[-1])
-        # ------------------------------------------------------------------------------------------
-        # Deprecated (slower, not sure numba is handled properly):
-        # t1, t2, o3t = [], [], []
-        # for i in prange(n_tpts):
-        #     t1.append(-0.5*commutator(o2t[i], At[i]))
-        #     t2.append((1.0/12.0)*commutator(o1t[i], commutator(o1t[i], At[i])))
-        # o3t_integrand = t1+t2
-        # for i in prange(n_tpts):
-        #     o3t.append(matrix_integral(times[0:i+1], o3t_integrand[0:i+1]))
-        # magnus_terms.append(o3t[-1])
-        # ------------------------------------------------------------------------------------------
 
     # Precompute the Omega_4(t) terms, integrating from t0 to t = t0, ..., t1
     if order >= 4:
@@ -142,11 +136,12 @@ def compute_magnus_terms(A: np.ndarray, t0: float, t1: float, n_tpts: Optional[i
                 + commutator(o2t[i], commutator(o1t[i], At[i])) for i in range(n_tpts)], axis=0
         )
         o4t_integrand = t1 + t2
-        if (integration_method == 'trapezoid'):
-            o4t = sp.integrate.cumulative_trapezoid(o4t_integrand, x=times, axis=0, initial=0)
-        elif (integration_method == 'simpson'):
-            # o4t = sp.integrate.cumulative_simpson(o4t_integrand, x=times, axis=0, initial=0)
-            o4t = integral_cumulative_simpson(times, o4t_integrand) 
+        o4t = integral_cumulative(o4t_integrand, x=times, axis=0, initial=0)
+        # if (integration_method == 'trapezoid'):
+        #     o4t = sp.integrate.cumulative_trapezoid(o4t_integrand, x=times, axis=0, initial=0)
+        # elif (integration_method == 'simpson'):
+        #     # o4t = sp.integrate.cumulative_simpson(o4t_integrand, x=times, axis=0, initial=0)
+        #     o4t = integral_cumulative_simpson(o4t_integrand, times) 
         magnus_terms.append(o4t[-1])
 
     # Precompute the Omega_5(t) terms, integrating from t0 to t = t0, ..., t1
@@ -162,11 +157,12 @@ def compute_magnus_terms(A: np.ndarray, t0: float, t1: float, n_tpts: Optional[i
             commutator(o1t[i], At[i])))) for i in range(n_tpts)], axis=0
         )
         o5t_integrand = t1 + t2 + t3
-        if (integration_method == 'trapezoid'):
-            o5t = sp.integrate.cumulative_trapezoid(o5t_integrand, x=times, axis=0, initial=0)
-        elif (integration_method == 'simpson'):
-            # o5t = sp.integrate.cumulative_simpson(o5t_integrand, x=times, axis=0, initial=0)
-            o5t = integral_cumulative_simpson(times, o5t_integrand)
+        o5t = integral_cumulative(o5t_integrand, x=times, axis=0, initial=0)
+        # if (integration_method == 'trapezoid'):
+        #     o5t = sp.integrate.cumulative_trapezoid(o5t_integrand, x=times, axis=0, initial=0)
+        # elif (integration_method == 'simpson'):
+        #     # o5t = sp.integrate.cumulative_simpson(o5t_integrand, x=times, axis=0, initial=0)
+        #     o5t = integral_cumulative_simpson(o5t_integrand, times)
         magnus_terms.append(o5t[-1])
 
     # Precompute the Omega_6(t) terms, integrating from t0 to t = t0, ..., t1
@@ -185,11 +181,12 @@ def compute_magnus_terms(A: np.ndarray, t0: float, t1: float, n_tpts: Optional[i
             + commutator(o2t[i], commutator(o1t[i], commutator(o1t[i], commutator(o1t[i], At[i]))))\
             for i in range(n_tpts)], axis=0)
         o6t_integrand = t1 + t2 + t3
-        if (integration_method == 'trapezoid'):
-            o6t = sp.integrate.cumulative_trapezoid(o6t_integrand, x=times, axis=0, initial=0)
-        elif (integration_method == 'simpson'):
-            # o6t = sp.integrate.cumulative_simpson(o6t_integrand, x=times, axis=0, initial=0)
-            o6t = matrix_integral_simpson(times, o6t_integrand)
+        o6t = integral_cumulative(o6t_integrand, x=times, axis=0, initial=0)
+        # if (integration_method == 'trapezoid'):
+        #     o6t = sp.integrate.cumulative_trapezoid(o6t_integrand, x=times, axis=0, initial=0)
+        # elif (integration_method == 'simpson'):
+        #     # o6t = sp.integrate.cumulative_simpson(o6t_integrand, x=times, axis=0, initial=0)
+        #     o6t = matrix_integral_simpson(o6t_integrand, times)
         magnus_terms.append(o6t[-1])
 
     return np.array(magnus_terms)
@@ -212,9 +209,11 @@ if __name__ == "__main__":
     def A(t):
         return np.array([[1.0*t, 2.0+3j*t],[2.0-3j*t, 2.0]])
     t0, t1 = 0.0, 1.0
-    exp_Omega = magnus_expansion(A, t0, t1, n_tpts=100, order=1, integration_method='trapezoid')
-    print(exp_Omega)
-    # exp_Omega = magnus_expansion(A, t0, t1, n_tpts=100, order=1, integration_method='simpson')
-    # print(exp_Omega)
+    exp_Omega_1 = magnus_expansion(A, t0, t1, n_tpts=100, order=6, integration_method='trapezoid')
+    print(exp_Omega_1)
+    exp_Omega_2 = magnus_expansion(A, t0, t1, n_tpts=100, order=6, integration_method='simpson')
+    print(exp_Omega_2)
+    print(exp_Omega_1 == exp_Omega_2)
+    print(exp_Omega_1-exp_Omega_2)
 
 
